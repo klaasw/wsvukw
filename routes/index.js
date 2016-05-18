f33fb5ef0efa61ba41037c33c7f753c61c1cde61
bernhard <bernhard.rischke@init.de>
2016-05-18 17:29:41 +0200

    Übertragen der Änderungen ukwDisplay nach ukwDisplayKlein und ukwDisplayGross, route für ukw_gr

--- routes/index.js
+++ routes/index.js
@@ -87,7 +87,7 @@ router.get('/ukw', function (req, res) {
     }
 }); //router Ende
 
-/* GET UKW Display Kleine Schaltflächen*/
+/* GET UKW Display Kleine Schaltflaechen*/
 router.get('/ukw_kl', function (req, res) {
     log.debug(req.ip);
     var benutzer = findeApNachIp(req.ip); //Arbeitsplatz aus Konfig lesen
@@ -98,6 +98,36 @@ router.get('/ukw_kl', function (req, res) {
             log.info(konfig.FunkstellenDetails[konfig.FunkstellenReihe['Button11'][0]]);
             //ukwDisplay --> zum Testen eines neuen Layouts
             res.render('ukwDisplayKlein', {
+
+                "gesamteKonfig": konfig
+
+            }); //res send ende
+        }); //erstelleKonfigFurAp Ende
+    } //if Ende
+
+    //kein Benutzer zu IP gefunden
+    else {
+        res.render('error', {
+            message: 'keine Benutzer konfiguriert zu IP: ' + req.ip,
+            error: {
+                status: 'kein'
+            }
+        })
+    }
+}); //router Ende
+
+
+/* GET UKW Display grosse Schaltflaechen*/
+router.get('/ukw_gr', function (req, res) {
+    log.debug(req.ip);
+    var benutzer = findeApNachIp(req.ip); //Arbeitsplatz aus Konfig lesen
+    if (benutzer) {
+        log.debug(FILENAME + ' Arbeitsplatz gefunden! IP: ' + req.ip);
+        erstelleKonfigFurAp(benutzer, function (konfig) {
+            //Uebergebe Funkstellen ID an Jade Template
+            log.info(konfig.FunkstellenDetails[konfig.FunkstellenReihe['Button11'][0]]);
+            //ukwDisplay --> zum Testen eines neuen Layouts
+            res.render('ukwDisplayGross', {
 
                 "gesamteKonfig": konfig
 
