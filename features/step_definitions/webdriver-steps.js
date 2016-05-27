9cc8d057bba6a8189867a70bd12b86dea6f48a85
bernhard <bernhard.rischke@init.de>
2016-05-27 14:09:30 +0200

    Testfälle für Mehrkanal-Anlagen, Einzel-/Gruppenschaltung erkennen/umschalten können, Mehrere Bedingungen / Prüfungen mit And, Given ist funktional gleich mit When

--- features/step_definitions/webdriver-steps.js
+++ features/step_definitions/webdriver-steps.js
@@ -1,10 +1,11 @@
 var myStepDefinitionsWrapper = function () {
     var cfg = require('../../cfg.js');
+    // var request  = require('co-request.js');
 
     this.When(/^Webseite "([^"]*)" aufgerufen wird$/, function (url) {
         var ip = cfg.cfgIPs.httpIP;
         var port = cfg.port;
-        var openUrl = "http://" + ip + ":" + port + url.replace("UKWDisplay",""); 
+        var openUrl = "http://" + ip + ":" + port + url.replace("UKWDisplay", "");
         browser.url(openUrl);
     });
 
@@ -18,14 +19,14 @@ var myStepDefinitionsWrapper = function () {
     });
 
     this.Then(/^wird in "([^"]*)" der Hintergrund "([^"]*)"$/, function (button, color) {
-        console.log("teste Hintergrundfarbe "+ color+" fuer Button: "+button);
+        console.log("\t\tteste Hintergrundfarbe " + color + " fuer Button: " + button);
         var styleExists = '#' + button + 'panel div.panel.panel-default';
         switch (color) {
             case "weiss":
                 styleExists = styleExists;
                 break;
             case "blau":
-                styleExists = styleExists+ ".panel-primary";
+                styleExists = styleExists + ".panel-primary";
                 break;
             case "gruen":
                 styleExists = "div#" + button + ".bg-success";
@@ -34,7 +35,7 @@ var myStepDefinitionsWrapper = function () {
                 styleExists = "div#" + button + ".bg-danger";
                 break;
         }
-        console.log("suche nach Element: "+styleExists);
+        console.log("\t\tsuche nach Element: " + styleExists);
         browser.waitForExist(styleExists, 15000);
     });
 
@@ -44,8 +45,9 @@ var myStepDefinitionsWrapper = function () {
         var ip = cfg.cfgIPs.httpIP;
         var port = cfg.port;
         var url = "http://" + ip + ":" + port + "/mockmessage?messageText=" + message;
-        console.log(" URL : " + JSON.stringify(url));
-        request(url, function (error, response, body) {
+        console.log("\t\tURL : " + JSON.stringify(url));
+        // yield
+            request(url).then(function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 var response = body;
                 console.log(" REST response: " + response);
@@ -60,9 +62,37 @@ var myStepDefinitionsWrapper = function () {
     });
 
 
+    this.When(/^SIPNachricht "([^"]*)" mit state "([^"]*)" an FunkstellenID von "([^"]*)" gesendet wird$/, function (nachrichtentyp, state, button) {
+        // Write the automation code here
+        // FunkstellenID aus der Arbeitsplatzkonfig heraus bestimmen (Hauptanalgen-ID), NebenanlagenID
+        pending();
+    });
+
+    this.When(/^Mehrkanalanlage "([^"]*)" auf Kanal "([^"]*)" umgeschaltet wird$/, function (button, channel) {
+        // Write the automation code here
+        pending();
+        // html body.fuelux div.container-fluid div.content div.col-md-12 div.row div#Button22panel.col-md-2 div.panel.panel-default div#Button22 h2#Button22.text-right button.btn.btn-default.btn-lg.dropdown-toggle
+        browser.click("h2#"+ button+ ".text-right button.btn.btn-default.btn-lg.dropdown-toggle");
+        // html body.fuelux.modal-open div.container-fluid div.content div.col-md-12 div#mkaModal.modal.fade.in div.modal-dialog div.modal-content div.modal-body p button.btn.btn-default.btn-lg
+
+        browser.waitForExist(styleExists, 15000);
+    });
+
+
+    this.Then(/^wird in "([^"]*)" der Kanal "([^"]*)" angezeigt$/, function (button, channel) {
+        browser.getText("h2#" + button).toEqual(channel);
+    });
+
+
+    this.When(/^Mehrkanalanlage "([^"]*)" auf Kanaleingabefeld "([^"]*)" eingegeben wird$/, function (arg1, arg2) {
+        // Write the automation code here
+        pending();
+    });
+
+
     this.When(/^SIPNachricht "([^"]*)" mit state "([^"]*)" an FunkstellenID "([^"]*)" und Kanal gesendet wird$/, function (nachrichtentyp, state, id, channel) {
         var message = "<" + nachrichtentyp + " id='" + id + "' state='" + state + "' channel='" + channel + "'/>";
-        console.log("message: " + message);
+        console.log("\t\tmessage: " + message);
         request("http://10.22.30.1:3000/mockmessage?messageText=" + message, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 var response = JSON.parse(body);
@@ -74,5 +104,17 @@ var myStepDefinitionsWrapper = function () {
     });
 
 
+    this.When(/^Navigationselement Gruppe_Einzel "([^"]*)" anzeigt$/, function (zustand) {
+        // li#gruppe a
+        expect(browser.getText('li#gruppe a')).toEqual(zustand);
+    });
+
+    this.When(/^Navigationselement Gruppe_Einzel aktiviert wird$/, function () {
+        browser.click('li#gruppe a');
+    });
+
+    this.Then(/^Test jetzt beenden.$/, function (){
+       browser.end();
+    });
 };
 module.exports = myStepDefinitionsWrapper;
--- features/webdriver.feature
+++ features/webdriver.feature
@@ -3,17 +3,51 @@ Feature: UKW Display austesten
   Als Benutzer des UKW Display
   teste ich die wesentlichen Funktionen der Seite
 
-  Scenario: Aufruf UKWDisplay
+  Scenario: Aufruf UKWDisplay für Arbeitsplatz "JA NvD"
     When  Webseite "UKWDisplay/" aufgerufen wird
     Then  lautet der Titel "UKW Display"
 
   Scenario: Schalte Button11 ein
+    Given Webseite "UKWDisplay/ukw_gr" aufgerufen wird
+    And   Webseite "UKWDisplay/ukw_kl" aufgerufen wird
     When  Button "Button11" geschaltet wird
     Then  wird in "Button11" der Hintergrund "blau"
 
   Scenario: Schalte Button11 aus
     When  Button "Button11" geschaltet wird
     Then  wird in "Button11" der Hintergrund "weiss"
+
+
+
+
+  Scenario: Schalte Button11 ein
+    When  Button "Button11" geschaltet wird
+    Then  wird in "Button11" der Hintergrund "blau"
+
+  Scenario: Nur der letzte Button wird blau
+    When  Button "Button12" geschaltet wird
+    Then  wird in "Button11" der Hintergrund "weiss"
+    And   wird in "Button12" der Hintergrund "blau"
+
+
+
+  Scenario: Mehrkanalanlage umschalten auf Kanal 5
+    When  Mehrkanalanlage "Button22" auf Kanal "5" umgeschaltet wird
+    Then  wird in "Button22" der Kanal "5" angezeigt
+
+  Scenario: Mehrkanalanlage umschalten auf Kanal 1 5
+    When  Mehrkanalanlage "Button22" auf Kanaleingabefeld "1 5" eingegeben wird
+    Then  wird in "Button22" der Kanal "15" angezeigt
+
+
+
+  Scenario: Gruppenschaltung aktivieren
+    Given Navigationselement Gruppe_Einzel "Einzelschaltung" anzeigt
+    When  Navigationselement Gruppe_Einzel aktiviert wird
+    Then  Navigationselement Gruppe_Einzel "Gruppenschaltung" anzeigt
+
+
+
 
   Scenario: Sende Mocknachricht
     When  SIPNachricht "RX" mit state "1" an FunkstellenID "1-H-RFD-BHVVTA-FKEK-1" gesendet wird
@@ -28,6 +62,10 @@ Feature: UKW Display austesten
     Then  wird in "Button11" der Hintergrund "gruen"
 
   Scenario: Sende Mocknachricht
+    When  SIPNachricht "TX" mit state "1" an FunkstellenID von "Button11" gesendet wird
+    Then  wird in "Button11" der Hintergrund "gruen"
+
+  Scenario: Sende Mocknachricht
     When  SIPNachricht "TX" mit state "0" an FunkstellenID "1-H-RFD-BHVVTA-FKEK-1" gesendet wird
     Then  wird in "Button11" der Hintergrund "weiss"
-
+    And   Test jetzt beenden
