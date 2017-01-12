1652545adb4f7966c5f8c884aaa45059d54f688a
rwild <regina.wild@init.de>
2017-01-12 16:03:25 +0100

    Unittest-Preparations

---
+++ test/appTest.js
@@ -0,0 +1,16 @@
+/**
+ * Created by rwild on 12.01.2017.
+ */
+
+
+var expect    = require("chai").expect;
+const app = require('../app.js');
+
+
+describe("Prüfe Funktionen app.js", function() {
+    it("Erreichbarkeit des Ports", function() {
+        var port = app.normalizePort(123)
+
+        expect(port).to.be.true;
+    });
+});
---
+++ test/ukwTest.js
@@ -0,0 +1,31 @@
+/**
+ * Created by rwild on 12.01.2017.
+ */
+
+var expect    = require("chai").expect;
+var assert = require('assert');
+const ukw = require('../ukw.js');
+
+
+describe("Auswertung/Weiterleitung der SIP Meldungen vom RFD", function() {
+    it("prüfe Erreichbarkeit des RFD WebServices", function() {
+        var rfdWS = ukw.pruefeRfdWS();
+        expect(ukw.pruefeRfdWS()).to.be.true;
+    });
+
+
+    it("sende SIP Nachricht", function() {
+        var res = ukw.sendeSipNachricht("OK", function (result, error) {
+            if (result == 'OK') {
+                return 'Abgesendet: ';
+            }
+            else {
+                return 'Fehler: ';
+            }
+            });
+
+        expect(res).equals('Abgesendet: ');
+    });
+
+
+});
