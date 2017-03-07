3b45d049777d02fee6f65c8845d116b75029d730
klaasw <klaas.wuellner@vm-ukw-xxx>
2017-03-07 12:52:31 +0000

    Timeout weiter erhoeht

--- features/_support/page-objects/ukw-widget.js
+++ features/_support/page-objects/ukw-widget.js
@@ -42,19 +42,19 @@ widgets.header = {
         switch(color) {
             case "Standard":
                 browser.click(stdTheme);
-                browser.pause(3000);
+                browser.pause(5000);
                 break;
             case "Flach":
                 browser.click(flatTheme);
-                browser.pause(3000);
+                browser.pause(5000);
                 break;
             case "Dunkel / marineblau":
                 browser.click(darklyTheme);
-                browser.pause(3000);
+                browser.pause(5000);
                 break;
             case "Dunkel / hellblau":
                 browser.click(cyborgTheme);
-                browser.pause(3000);
+                browser.pause(5000);
                 break;
         }
     },
