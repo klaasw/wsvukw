b95c537386809eeaf3f110bab672000728f187fe
Felix <felix@waldbeek.com>
2017-04-19 08:00:04 +0200

    code format

--- cfg.js
+++ cfg.js
@@ -8,9 +8,9 @@
 
 'use strict';
 
-const fs = require('fs'); // Zugriff auf das Dateisystem
+const fs              = require('fs'); // Zugriff auf das Dateisystem
 const nodeJsSIPsocket = require('jssip-node-websocket'); //JsSIP Node Websocket Modul
-const HOSTNAME = require('os').hostname();
+const HOSTNAME        = require('os').hostname();
 
 // TODO: ungenutzte variable löschen?
 let AKTUELLER_SERVER = ''; //globale Variable für aktuellen Server. Einbindung in Konfig zur Darstellung des aktuellen Server via Jade Template layout.jade
@@ -43,8 +43,8 @@ function getIPs() {
 const cfgIPs = getIPs();
 console.log(cfgIPs);
 
-const jsSIPsocketDUE =  new nodeJsSIPsocket('ws://' + cfgIPs.sipIP + ':10080');
-const jsSIPsocketMockRFD =  new nodeJsSIPsocket('ws://' + cfgIPs.sipIP + ':10080');
+const jsSIPsocketDUE     = new nodeJsSIPsocket('ws://' + cfgIPs.sipIP + ':10080');
+const jsSIPsocketMockRFD = new nodeJsSIPsocket('ws://' + cfgIPs.sipIP + ':10080');
 
 /**
  * Globale Serverkonfiguration
@@ -79,41 +79,43 @@ const jsSIPsocketMockRFD =  new nodeJsSIPsocket('ws://' + cfgIPs.sipIP + ':10080
 const cfg = {
 	'urlRFDWebservice': 'http://' + cfgIPs.rfdIP + ':8789/I_RFD_DUE_Steuerung',
 
-	'jsSipConfiguration_DUE': {
-		'sockets': [jsSIPsocketDUE],
-		'uri': 'sip:due@' + cfgIPs.sipIP + ':5060',
+	'jsSipConfiguration_DUE':     {
+		'sockets':  [jsSIPsocketDUE],
+		'uri':      'rfd@' + cfgIPs.sipIP + ':5060',
 		// TODO fuer unterschiedliche Passwoerter dev/stage/prod: noch in serverIPs auslagern
-		'password': 'due'
+		'password': 'rfd',
+		register:   true
 	},
 	'jsSipConfiguration_mockRFD': {
-		'sockets': [jsSIPsocketMockRFD],
-		'uri': 'sip:rfd@' + cfgIPs.sipIP + ':5060',
+		'sockets':  [jsSIPsocketMockRFD],
+		'uri':      'rfd@' + cfgIPs.sipIP + ':5060',
 		// TODO fuer unterschiedliche Passwoerter dev/stage/prod: noch in serverIPs auslagern, unterschiedliche Passwoerter vergeben, mindestens produktiv
-		'password': 'rfd'
+		'password': 'rfd',
+		register:   true
 	},
 
-	'mongodb': cfgIPs.mongoDbs,
+	'mongodb':    cfgIPs.mongoDbs,
 	'replicaSet': cfgIPs.replicaSet || 'dueReplicaSet',
-	'auth': cfgIPs.auth,
-	'auth_user': cfgIPs.auth_user || 'ukwserver',
-	'auth_pw': cfgIPs.auth_pw,
+	'auth':       cfgIPs.auth,
+	'auth_user':  cfgIPs.auth_user || 'ukwserver',
+	'auth_pw':    cfgIPs.auth_pw,
 
 	//HTTP Port für die nodeJS Instanz
-	'port': cfgIPs.port || '3000',
+	'port':       cfgIPs.port || '3000',
 	'configPath': 'config/',
 
 	// 0 = Überprüfung abschalten
-	'intervall': cfgIPs.checkRfdIntervallInSeconds * 1000 || 0,
-	'alternativeIPs': cfgIPs.alternativeServer,
-	'aktuellerServer': AKTUELLER_SERVER,
+	'intervall':         cfgIPs.checkRfdIntervallInSeconds * 1000 || 0,
+	'alternativeIPs':    cfgIPs.alternativeServer,
+	'aktuellerServer':   AKTUELLER_SERVER,
 	'aktuellerHostname': HOSTNAME,
 	cfgIPs,
 
 	loglevelConsole: cfgIPs.loglevelConsole || 'debug',
-	loglevelFile: cfgIPs.loglevelFile || 'info',
+	loglevelFile:    cfgIPs.loglevelFile || 'info',
 
 	displaySperreTimeout: 10, // Dauer der Displaysperre in Sekunden
-	atisKennungTimeout: 5 // Anzeige der ATIS Kennung in Sekunden
+	atisKennungTimeout:   5 // Anzeige der ATIS Kennung in Sekunden
 };
 
 module.exports = cfg;
